1bd824f5c0cf2e511bf16997e1721d317b6bf4cc
vollborn <oliver.vollborn@gmail.com>
2022-12-08 10:07:39 +0100

    Ignore: move options routes out of auth middleware

--- routes.php
+++ routes.php
@@ -21,9 +21,15 @@ $empty = static function () {
 
 SimpleRouter::post($url . '/login', [LoginController::class, 'login']);
 
+SimpleRouter::options($url . '/persons', $empty);
+SimpleRouter::options($url . '/customers', $empty);
+SimpleRouter::options($url . '/pitches', $empty);
+SimpleRouter::options($url . '/additional-costs', $empty);
+SimpleRouter::options($url . '/bookings', $empty);
+
 SimpleRouter::group([
     'middleware' => AuthMiddleware::class
-], static function () use ($url, $empty) {
+], static function () use ($url) {
     SimpleRouter::get($url . '/countries', [CountryController::class, 'index']);
     SimpleRouter::get($url . '/additional-cost-types', [AdditionalCostTypeController::class, 'index']);
 
@@ -32,28 +38,24 @@ SimpleRouter::group([
     SimpleRouter::post($url . '/customers', [CustomerController::class, 'store']);
     SimpleRouter::put($url . '/customers', [CustomerController::class, 'update']);
     SimpleRouter::delete($url . '/customers', [CustomerController::class, 'delete']);
-    SimpleRouter::options($url . '/customers', $empty);
 
     SimpleRouter::get($url . '/pitches', [PitchController::class, 'index']);
     SimpleRouter::get($url . '/pitches/show', [PitchController::class, 'show']);
     SimpleRouter::post($url . '/pitches', [PitchController::class, 'store']);
     SimpleRouter::put($url . '/pitches', [PitchController::class, 'update']);
     SimpleRouter::delete($url . '/pitches', [PitchController::class, 'delete']);
-    SimpleRouter::options($url . '/pitches', $empty);
 
     SimpleRouter::get($url . '/additional-costs', [AdditionalCostController::class, 'index']);
     SimpleRouter::get($url . '/additional-costs/show', [AdditionalCostController::class, 'show']);
     SimpleRouter::post($url . '/additional-costs', [AdditionalCostController::class, 'store']);
     SimpleRouter::put($url . '/additional-costs', [AdditionalCostController::class, 'update']);
     SimpleRouter::delete($url . '/additional-costs', [AdditionalCostController::class, 'delete']);
-    SimpleRouter::options($url . '/additional-costs', $empty);
 
     SimpleRouter::get($url . '/bookings', [BookingController::class, 'index']);
     SimpleRouter::get($url . '/bookings/show', [BookingController::class, 'show']);
     SimpleRouter::post($url . '/bookings', [BookingController::class, 'store']);
     SimpleRouter::put($url . '/bookings', [BookingController::class, 'update']);
     SimpleRouter::delete($url . '/bookings', [BookingController::class, 'delete']);
-    SimpleRouter::options($url . '/bookings', $empty);
 
     SimpleRouter::get($url . '/bookings/persons', [BookingPersonController::class, 'index']);
     SimpleRouter::get($url . '/bookings/price', [BookingPriceController::class, 'show']);
@@ -63,5 +65,4 @@ SimpleRouter::group([
     SimpleRouter::post($url . '/persons', [PersonController::class, 'store']);
     SimpleRouter::put($url . '/persons', [PersonController::class, 'update']);
     SimpleRouter::delete($url . '/persons', [PersonController::class, 'delete']);
-    SimpleRouter::options($url . '/persons', $empty);
 });
